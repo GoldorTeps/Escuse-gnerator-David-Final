2070d9450a6a710412ab455aa632f5af05dee764
Goldor <goldorteps@gmail.com>
2023-05-30 17:48:14 +0200

    cargada la función. ¿Dónde va?

--- src/app.js
+++ src/app.js
@@ -9,3 +9,19 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+// // Primer paso: declaro mi funcion
+// function myApp() {
+//   let who = ['The dog','My grandma','His turtle','My bird'];
+//   let action = ['ate','peed','crushed','broke'];
+//   let what = ['my homework', 'the keys', 'the car'];
+//   let when = ['before the class','right on time','when I finished','during my lunch','while I was praying'];
+//   const whoRandom = who[Math.floor(Math.random() * who.length)];
+//   const actionRandom = action[Math.floor(Math.random() * action.length)];
+//   const whatRandom = what[Math.floor(Math.random() * what.length)];
+//   const whenRandom = when[Math.floor(Math.random() * when.length)];
+//   const onLoad = whoRandom + " " + actionRandom + " " +  whatRandom + " " + whenRandom;
+//   console.log(onLoad)
+//   }
+//   // Segundo Paso: ejecuto mi funcion
+//   myApp()
